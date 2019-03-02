9b49a56c3f04f9b24622fdddfecd8d7c45c836b5
Yichao Zhou <broken.zhou@gmail.com>
2019-03-01 17:49:29 -0800

    initial commit

---
+++ sudo.plugin.zsh
@@ -0,0 +1,10 @@
+sudo-command-line() {
+    [[ -z $BUFFER ]] && zle up-history
+    if [[ $BUFFER == sudo\ * ]]; then
+        LBUFFER="${LBUFFER#sudo }"
+    else
+        LBUFFER="sudo $LBUFFER"
+    fi
+}
+zle -N sudo-command-line
+bindkey "\es" sudo-command-line
